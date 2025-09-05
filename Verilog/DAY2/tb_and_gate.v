0b3f3c0be355e9b5bdaf570f2e82d510d5cc4e91
Sarulatha2004 <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-05 18:57:41 +0530

    Add files via upload

---
+++ Verilog/DAY2/tb_and_gate.v
@@ -0,0 +1,20 @@
+module tb_and;
+
+  reg a, b;
+  wire o;
+  and_gate uut (.a(a), .b(b), .o(o));
+
+  initial begin
+    $dumpfile("and_wave.vcd");
+    $dumpvars(0, tb_and);
+
+    $display("time | a b | o");
+    $monitor("%4t | %b %b | %b", $time, a, b, o);
+
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+endmodule
