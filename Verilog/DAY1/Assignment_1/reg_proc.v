450eba7a743e5258c7561ed4e1303e5799e69fe7
Sarulatha2004 <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-05 11:49:02 +0530

    Delete Verilog/DAY1/Assignment_1 directory

--- Verilog/DAY1/Assignment_1/reg_proc.v
+++
@@ -1,9 +0,0 @@
-module reg_proc;
-reg a,b;
-initial begin
-a=1'b1;
-b=a;
-$display("time=%0t a=%b b=%b",$time,a,b);
-$finish;
-end 
-endmodule
--- Verilog/DAY1/Assignment_1/wire_assign.v
+++
@@ -1,9 +0,0 @@
-module wire_assign;
-wire a,b;
-assign a=1'b1;
-assign b=a;
-initial begin
-$display("Time=%0t a=%b b=%b",$time,a,b);
-$finish;
-end 
-endmodule
