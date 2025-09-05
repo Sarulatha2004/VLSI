8770b989389cf90380c82e855b7334ce0e104402
Sarulatha2004 <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-05 12:20:43 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/Assignment_1/11.Manipulate Vector Using Part Select/part_sel.v
@@ -0,0 +1,16 @@
+module manipulate_part_sel;
+reg [7:0]bus;
+reg [7:0]busA;
+initial begin
+bus=8'b01010101;
+busA=8'b00000000;
+for(integer i=0;i<=7;i++)
+begin
+busA[i]=bus[i];
+end
+$display("time=%0t : busA=%b ",$time,busA);
+busA[3:0]=bus[3:0];
+$display("time=%0t : busA=%b ",$time,busA);
+$finish;
+end
+endmodule
