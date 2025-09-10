35e3671b17f2040622872e3668a1cea68b2a8cfb
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-10 22:37:14 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/Assignment_5/3.Race Conditions & Scheduling/C22.v
@@ -0,0 +1,23 @@
+module race_blocking;
+  reg a, b, clk;
+
+  always @(posedge clk) begin
+    a = b;   
+  end
+
+  always @(posedge clk) begin
+    b = a;  
+  end
+
+  initial begin
+    clk = 0; a = 1; b = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $monitor("Time=%0t clk=%b a=%b b=%b",$time,clk,a,b);
+    #20 $finish;
+  end
+endmodule
+
+
