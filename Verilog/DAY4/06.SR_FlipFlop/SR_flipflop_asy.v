6f2c2f91d553505a305012a58a19f27d9f0a62d2
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-15 15:32:03 +0530

    Add files via upload

---
+++ Verilog/DAY4/06.SR_FlipFlop/SR_flipflop_asy.v
@@ -0,0 +1,34 @@
+module sr_ff_async_reset (
+    input  clk,
+    input  reset,  
+    input  S,
+    input  R,
+    output reg Q,
+    output reg Qb
+);
+
+  always @ (posedge clk or posedge reset) begin
+    if (reset) begin
+      Q  <= 1'b0;  
+      Qb <= 1'b1;
+    end
+    else begin
+      case ({S,R})
+        2'b00: ;            
+        2'b01: begin 
+                 Q <= 1'b0;
+                 Qb <= 1'b1; 
+               end 
+        2'b10: begin 
+                 Q <= 1'b1;  
+                 Qb <= 1'b0;
+               end 
+        2'b11: begin 
+                 Q <= 1'bx; 
+                 Qb <= 1'bx;
+               end 
+      endcase
+    end
+  end
+endmodule
+
---
+++ Verilog/DAY4/06.SR_FlipFlop/SR_flipflop_asy_tb.v
@@ -0,0 +1,30 @@
+module tb_sr_ff;
+  reg clk, reset, S, R;
+  wire Q, Qb;
+
+  sr_ff_async_reset u1 (.clk(clk), .reset(reset), .S(S), .R(R), .Q(Q), .Qb(Qb));
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk; 
+  end
+
+  initial begin
+    $monitor("Time=%4t |  clk=%b    reset=%b    S=%b  R=%b |   Q= %b|  Qb=%b",
+              $time, clk, reset, S, R, Q, Qb);
+
+    reset=1; S=0; R=0; #12; 
+    reset=0;
+
+    S=1; R=0; #20; 
+    S=0; R=1; #20; 
+    S=0; R=0; #20; 
+    S=1; R=1; #20;
+
+    reset=1; #7;    
+    reset=0; #20;   
+
+    $finish;
+  end
+endmodule
+
