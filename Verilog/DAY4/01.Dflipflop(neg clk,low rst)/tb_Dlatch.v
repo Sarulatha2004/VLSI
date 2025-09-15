b24b84de6538b56ff7a3918092b91ffd18ed0fb7
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-15 13:28:28 +0530

    Add files via upload

---
+++ Verilog/DAY4/01.Dflipflop(neg clk,low rst)/tb_Dlatch.v
@@ -0,0 +1,17 @@
+module d_latch_tb;
+  reg d,en;
+  wire q,qbar;
+  Dlatch uut (.d(d),.en(en),.q(q),.qbar(qbar));
+  initial begin
+    $dumpfile("Dlatch_wave.vcd");
+    $dumpvars;
+    $monitor ("Time=%0t | d=%b | en=%b | q=%b  | qbar=%b",$time,d,en,q,qbar);
+    
+   #10; en=0; d=1;
+   #10;       d=0;
+   #10; en=1; d=1;
+   #10;       d=0;
+   #10;
+   $finish;
+  end 
+endmodule
