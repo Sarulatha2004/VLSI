b43f821bf59d7b8295bfbacd84ccd7e3340dfef5
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-09-17 22:37:49 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/Assignment_7/B.Functions/Qn_22/odd_parity.v
@@ -0,0 +1,28 @@
+module odd_parity;
+  reg [7:0] data;
+  reg parity;
+
+  function parity_fn(input [7:0] d);
+    integer i;
+    begin
+    parity_fn = 0;
+    for (i = 0; i < 8; i = i + 1) 
+    begin
+        parity_fn = parity_fn ^ d[i];
+    end
+    end
+  endfunction
+
+  initial begin
+    $monitor("Time=%0t | data=%b | parity=%b", $time, data, parity);
+
+    data = 8'b00000000; parity = parity_fn(data); #10;
+    data = 8'b00000001; parity = parity_fn(data); #10;
+    data = 8'b10101010; parity = parity_fn(data); #10;
+    data = 8'b11110010; parity = parity_fn(data); #10;
+    data = 8'b11111101; parity = parity_fn(data); #10;
+
+    #50 $finish;
+  end
+endmodule
+
