f616b2a3fcbe0fdfd851c6f780b550cb9dc469a7
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-10-12 12:26:50 +0530

    Add files via upload

---
+++ Verilog/DAY7/1001_seq_mealy_nonoverlap_tb.v
@@ -0,0 +1,35 @@
+module mealy_1001_nonoverlap_tb;
+  reg clk,rst,x;
+  wire y;
+  
+  mealy_1001_nonoverlap uut(.clk(clk), .rst(rst), .x(x), .y(y));
+
+  initial clk = 0;   
+  always #5 clk = ~clk;
+    
+  initial begin
+    $dumpfile("mealy_1001_nonoverlap.vcd");
+    $dumpvars;
+    $monitor ("TIME = %0t | CLK = %b | RST = %b | STATE = %b | NEXT_STATE = %b | X = %b | Y = %b " , $time,clk,rst,uut.state,uut.next_state,x,y);
+    x = 0;
+    #5 rst = 1;
+    #5 rst = 0;
+    
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10;
+    $finish;
+  end
+endmodule
---
+++ Verilog/DAY7/1001_seq_mealy_overlap_tb.v
@@ -0,0 +1,35 @@
+module mealy_1001_overlap_tb;
+  reg clk,rst,x;
+  wire y;
+  
+  mealy_1001_overlap uut(.clk(clk), .rst(rst), .x(x), .y(y));
+
+  initial clk = 0;   
+  always #5 clk = ~clk;
+    
+  initial begin
+    $dumpfile("mealy_1110_overlap.vcd");
+    $dumpvars;
+    $monitor ("TIME = %0t | CLK = %b | RST = %b | STATE = %b | NEXT_STATE = %b | X = %b | Y = %b " , $time,clk,rst,uut.state,uut.next_state,x,y);
+    x = 0;
+    #5 rst = 1;
+    #5 rst = 0;
+    
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 0;
+    #10 x = 1;
+    #10 x = 1;
+    #10 x = 1;
+    #10;
+    $finish;
+  end
+endmodule
